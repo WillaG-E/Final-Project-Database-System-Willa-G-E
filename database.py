2fc1e41687850306739638360d377bd8995642ec
WillaG-E <willa.galipeaueldridge@maine.edu>
2025-12-16 14:41:05 -0500

    Updated files for base code to run user input commands

--- database.py
+++ database.py
@@ -1,20 +1,65 @@
 #Author: Willa Galipeau-Eldridge
-#Date: 12/14/2025
+#Date: 12/16/2025
 #Purpose: 
 
+from recordStorage import Storage
+from indexManager import IndexManager
+from records import Record
 import csv
 
 class Database:
-    def __init__(self, searchableFields, initialIndexedField):
-        self
+    def __init__(self, searchableFields):
+        self.storage = Storage()
+        self.indexManager = IndexManager(searchableFields)
     
     def load_csv(self, path):
-        records = []
+        self.storage.records = []
+        self.indexManager = IndexManager(self.indexManager.searchableFields)
 
         with open(path) as f:
             reader = csv.reader(f)
-            next(reader)
-            for row in reader:
-                record = records(row[0], row[1], row[2], row[3], row[4])
-                self.records.append(record)
+            header = next(reader)
+            Record.FIELD_NAMES = header
+            
+            for index, row in enumerate(reader):
+                rec = Record(row)
+                self.storage.records.append(rec)
 
+        for index, rec in enumerate(self.storage.records):
+            self.indexManager.add_hash_record(rec, index)
+
+    def create_index(self, field):
+        records_with_indices = [(r, i) for i, r in enumerate(self.storage.records) if r != None]
+        self.indexManager.create_bplus_index(field, records_with_indices)
+
+    def get_record_indices(self, recordIndices):
+        records = []
+        for i in recordIndices:
+            if (i < len(self.storage.records)):
+                record = self.storage.records[i]
+                if (record != None):
+                    records.append(record)
+        return records
+    
+    def exact_search(self, field, value):
+        hash = self.indexManager.hashTables[field]
+        recordIndices = hash.search(value)
+        return self.get_record_indices(recordIndices)
+    
+    def range_search(self, field, low, high):
+        if field not in self.indexManager.bplusIndices:
+            raise Exception("Field is not indexed.")
+        
+        tree = self.indexManager.bplusIndices[field]
+        recordIndices = tree.range_search(low, high)
+        return self.get_record_indices(recordIndices)
+    
+    def delete_records(self, records):
+        for r in records:
+            try:
+                recordIndex = self.storage.records.index(r)
+            except ValueError:
+                continue
+
+            self.indexManager.delete_all(r, recordIndex)
+            self.storage.deleteRecords([r])
--- main.py
+++ main.py
@@ -2,26 +2,19 @@
 #Date: 12/16/2025
 #Purpose: Menu for Database System and user input
 
+from database import Database
+from records import Record
+
 def main():
 
     SEARCHABLE_FIELDS = ["movie_title","release_date", "genre", "rating", "box_office_revenue"]
-    
+    database = Database(searchableFields = SEARCHABLE_FIELDS)
     results = [] #stores the set of objects returned by a search
+    
     print("\n================ Database System ================")
     print(f"\nSearchable Fields: {','.join(SEARCHABLE_FIELDS)}")
-    print("""
-        1 - Load CSV
-        2 - Create Index
-        3 - Exact Search
-        4 - Range Search
-        5 - Export
-        6 - Delete
-        0 - Exit the program
-            """)
 
-    user_input = input("Enter a command: ")
-
-    while(user_input < "7" and user_input > "-1"):
+    while(True):
         print("""
         1 - Load CSV
         2 - Create Index
@@ -31,35 +24,54 @@ def main():
         6 - Delete
         0 - Exit the program
             """)
+        
+        user_input = input("Enter a command: ")
+
         if (user_input == "1"):
             #Load the data from the CSV file (MOCKDATA.CSV)
-            pass
+            path = "MOCK_DATA.csv"
+            database.load_csv(path)
+            print("Data is loaded and indexed by hash table.")
         elif (user_input == "2"):
             #Create a field to index
-            pass
+            field = input("Field to index (must be searchable field): ")
+            if (field in SEARCHABLE_FIELDS):
+                database.create_index(field)
+                print(f"Index created on {field}")
+            else:
+                    print("Invalid field name.")
         elif (user_input == "3"):
             #Exact search
             field = input("Search field: ")
             value = input("Search value: ")
 
             #function that sends the field and value variables to get an exact search
-            
-            print(results)
-
+            results = database.exact_search(field, value)
+            print(f"Found {len(results)} records.")
+            for r in results:
+                print(r)
         elif (user_input == "4"):
             #Range queries
-            pass
+            field = input("Indexed field: ")
+            low = float(input("Low bound: "))
+            high = float(input("High bound: "))
+            results = database.range_search(field, low, high)
+            print(f"Found {len(results)} records.")
         elif (user_input == "5"):
             #Export functionality
             filename = input("Export filename: ")
             with open(filename, "w") as f:
                 for row in results:
-                    f.write(",".join(row))
+                    f.write(",".join(row.csvRow()) + "\n")
             print(f"Successfully exported file: ${filename}")
-            pass
         elif (user_input == "6"):
             #Delete functionality
-            pass
+            if (results == None):
+                print("No search results to delete")
+                continue
+            database.delete_records(results)
+            print(f"Deleted {len(results)} records.")
+            results = [] #clears the results after deletion
         elif (user_input == "0"):
             return
 
